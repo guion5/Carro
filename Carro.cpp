ebf3c14f119927c7cba2c28e645dcc94ff5aad70
guion5 <127799018+guion5@users.noreply.github.com>
2023-04-24 20:26:34 -0300

    Update Carro.cpp

--- Carro.cpp
+++ Carro.cpp
@@ -1,29 +1,52 @@
 #include <iostream>
 using namespace std;
+//classe
 class Carro
 {
     public:
-    
+    //variáveis
     string Cor;
     string Modelo;
     float VelMax;
-    void Buzinar ();
-    };
+    
+    //método
+    void Buzina1 ();
+    void Buzina2 ();
+      
+    //construtor personalizado  
+    Carro()
+    {
+    VelMax = 220.f;
+    Modelo = "Camaro";
+    Cor = "Amarelo";
+    }
+};
     
     int main()
     {
         Carro MeuCarro;
+        Carro Carro2;
         
         MeuCarro.Cor = "Branco";
         MeuCarro.Modelo = "Fusca";
         MeuCarro.VelMax = 150.0f;
         
-        MeuCarro.Buzinar();
+        MeuCarro.Buzina1();
         cout << MeuCarro.Cor << endl;
         cout << MeuCarro.VelMax << endl;
         cout << MeuCarro.Modelo << endl;
-    }
-
-void Carro::Buzinar(){
+        
+        Carro2.Buzina2();
+        cout << Carro2.Cor << endl;
+        cout << Carro2.VelMax << endl;
+        cout << Carro2.Modelo << endl;
+}
+        
+void Carro::Buzina1(){
     cout << "Bi, bi!!!" << endl;
 }
+void Carro::Buzina2(){
+    cout << "Fon, fon!!!" << endl;
+    }
+    
+
